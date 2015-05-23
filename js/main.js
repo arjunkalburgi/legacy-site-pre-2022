0398e7e3cd00f762d84efd0e416c701871941dff
Arjun Kalburgi <askalburgi@gmail.com>
2015-05-23 17:27:31 -0600

    css fixes for a slightly more responsive site

--- js/main.js
+++ js/main.js
@@ -1,21 +1,15 @@
 function contentHeight() {
-	var height = 0;
-
-	// grab body height
-	var body = window.document.body.clientHeight;
-
-	// remove header height
-	var header = $(".header-container")[0].clientHeight;
-
-	// remove the amount wanted for the footer
-	var footer = 476;
-
-	// remove the 60px for the padding 
-	var padding = 60;
-
-	// summation 
-	height = body - header - footer - padding
-
+	var height = $(window).height() - 135;
 	// set height
-	var content = $(".content")[0].style.height = String(height) + "px";
+	$(".content")[0].style.height = String(height) + "px";
+}
+
+function projectscrollHeight() {
+	var height = $(window).height() * 0.58;
+	$(".workhorizontalscroll")[0].style.height = String(height) + "px";	
+}
+
+function morescrollHeight() {
+	var height = $(window).height() * 0.81;
+	$(".morehorizontalscroll")[0].style.height = String(height) + "px";	
 }
