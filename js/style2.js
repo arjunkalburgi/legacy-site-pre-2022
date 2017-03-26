be6b848dfd2c5100d4ce6e82dbc590bc7b548af4
Arjun Kalburgi <askalburgi@gmail.com>
2017-03-26 14:12:21 -0600

    background feature on ventures and some other small messy things

---
+++ js/style2.js
@@ -0,0 +1,27 @@
+var didScroll = false;
+var offset = $(window).height()*0.3; // size of window from top to when you want the first switch to be
+var blocksize = $(window).height()*0.9;  // this is programmed in 
+var ventures = ["eceweekhacked2017", "AUcourses", "howtocompe", "eceweekhacked2015"]
+
+$(window).scroll(function() {
+    didScroll = true;
+});
+
+setInterval(function() {
+    if ( didScroll ) {
+        didScroll = false;
+        var scroll = $(window).scrollTop(); 
+        var $background = $("#background");
+        var classToAdd = ""; 
+
+        var currprojectnum = Math.floor((scroll-offset)/blocksize); 
+        // console.log("scroll: "+ scroll + ", currprojectnum: " + currprojectnum); 
+        classToAdd = ventures[currprojectnum]; 
+
+        //Removing all classes 
+        $background.removeClass();
+
+        //Add new class 
+        $background.addClass(classToAdd,1000);
+    }
+}, 50);
