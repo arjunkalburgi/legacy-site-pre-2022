fb15a6dc64a2d6432e6f506214481b035076ff04
Arjun Kalburgi <askalburgi@gmail.com>
2017-03-26 18:00:25 -0600

    more ventures

--- js/style2.js
+++ js/style2.js
@@ -1,21 +1,20 @@
-var didScroll = false;
-var offset = $(window).height()*0.25; // size of window from top to when you want the first switch to be
-var blocksize = $(window).height()*0.9;  // this is programmed in 
-var ventures = ["eceweekhacked2017", "AUcourses", "howtocompe", "eceweekhacked2015"]
 
 $(window).scroll(function() {
+	var offset = $(window).height()*0.25; // size of window from top to when you want the first switch to be
+	var blocksize = $(window).height()*0.9;  // this is programmed in 
+	var projs = $(".block").map(function() { return this.id; }).toArray();
 	var scroll = $(window).scrollTop(); 
 	var $background = $("#background");
 	var classToAdd = ""; 
 
 	var currprojectnum = Math.floor((scroll-offset)/blocksize); 
-	// console.log("scroll: "+ scroll + ", currprojectnum: " + currprojectnum); 
-	classToAdd = ventures[currprojectnum]; 
+	classToAdd = projs[currprojectnum]; 
+	if (currprojectnum >= projs.length) {
+		console.log("hey"); 
+		classToAdd = projs[currprojectnum-1]; 
+	}
+	console.log("scroll: "+ scroll + ", currprojectnum: " + currprojectnum); 
 
-	//Removing all classes 
 	$("#background").removeClass();
-
-	//Add new class 
-	console.log(classToAdd); 
 	$("#background").addClass(classToAdd);
 });
