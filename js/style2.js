58aee0a9424a6b32dffed4b1fbe28645557147aa
Arjun Kalburgi <askalburgi@gmail.com>
2017-03-26 20:53:38 -0600

    projects page redone

--- js/style2.js
+++ js/style2.js
@@ -4,7 +4,6 @@ $(window).scroll(function() {
 	var blocksize = $(window).height()*0.9;  // this is programmed in 
 	var projs = $(".block").map(function() { return this.id; }).toArray();
 	var scroll = $(window).scrollTop(); 
-	var $background = $("#background");
 	var classToAdd = ""; 
 
 	var currprojectnum = Math.floor((scroll-offset)/blocksize); 
